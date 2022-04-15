a84578f5832a153ee77060b90beee51ea689becd
KTheXIII <pratchaya.k.git@gmail.com>
2022-04-16 01:27:15 +0200

    Send in file to run in the build script instead

--- Models/run.sh
+++ Models/run.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env sh
 
-source ./build.sh
-
 target=$1
+
+if [ -z "${1}" ]
+then
+    echo "No file to run"
+    exit 1
+fi
+
+./build.sh $target
+
 target="${target%.*}"
 args="${@:2}"
 
